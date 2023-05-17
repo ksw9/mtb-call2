962e5c97b3197324c9a76c43f538238e74f6f987
Katharine S. Walter <ksw9@users.noreply.github.com>
2023-05-17 14:50:44 -0600

    Create write_input_reads_list.sh

---
+++ scripts/write_input_reads_list.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#####################################################
+#### Create input reads list from data directory ####
+#####################################################
+## Create reads list input with full paths to benchmark data.
+
+DATA_DIR=$1 # path to data directory
+list_name=$2 # name of input reads list
+suffix_1=${3:-_R1_001.fastq.gz} # FASTQ_1 suffix
+batch=${list_name} # defines batch as list_name
+
+# Define suffix_2
+suffix_2=${suffix_1/R1/R2}
+
+# Create list with headers
+echo -e "sample\tfastq_1\tfastq_2\tbatch" > resources/input/${list_name}.tsv
+
+# Populate list
+for fastq_1 in ${DATA_DIR}/*${suffix_1}; do
+  fastq_2=${fastq_1/$suffix_1/$suffix_2}
+  echo $fastq_2
+  samp=$(basename ${fastq_1/suffix_1})
+  echo -e "${samp}\t${fastq_1}\t${fastq_2}\t${batch}" >> resources/input/${list_name}.tsv
+done
