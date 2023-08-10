cee19767f528e4973f8142bbafcf27c5c9447cac
Katharine Walter <u6045141@notchpeak2.int.chpc.utah.edu>
2023-08-10 14:28:55 -0600

    updated submission script

--- scripts/submit_mtb_pipeline.sh
+++ scripts/submit_mtb_pipeline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=10:00:00
 #SBATCH --nodes=1
 #SBATCH --account=walter
-#SBATCH --partition=notchpeak
+#SBATCH --partition=notchpeak-shared
 #SBATCH -o mtb-call_run-out-%j
 #SBATCH -e mtb-call_run-err-%j
 #SBATCH --mail-user=katharine.walter@hsc.utah.edu
@@ -10,12 +10,12 @@
 
 module load nextflow/20.10 singularity/3.8.7
 
-export WORKDIR=/uufs/chpc.utah.edu/common/home/walter-group1/tb/mtb-call2
+export WORKDIR=/uufs/chpc.utah.edu/common/home/walter-group2/tb/mtb-call2
 export TMPDIR=/scratch/general/nfs1/u6045141/tmp
 export NXF_SINGULARITY_CACHEDIR=$WORKDIR/images
 
 echo "Job started at $(date)"
 cd $WORKDIR
-nextflow run main.nf -profile singularity
+nextflow run main.nf -profile singularity -resume
 cd $HOME
 echo "Job ended at $(date)"
